6a7f5369beef8123ee5a5b5621fa0bf5dbd7f034
Erasmo J. Rojas G <erasmorojastech@gmail.com>
2022-06-30 17:08:00 -0400

    Add html, styles and script to the project
    Add html template that contains only a title, a subtitle and a div that
    will contain all the 16x16 squares to form a grid.
    
    Add reset.css that overrides default styles from some elements.
    
    Add styles.css for giving styles to the board and its squares.
    
    Add a script that construct a perfect board that is made of 16x16
    squares.

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const $board = document.querySelector('#board'),
+  $children = [];
+
+for (let i = 0; i < 256; i++) {
+
+  // Create a new element
+  let $square = document.createElement("div")
+  $square.setAttribute('class', 'square');
+  $square.style.cssText = `width: 32px; height: 32px;`
+
+  // Creating a tree of elements
+  $children.push($square);
+}
+
+// Adding the tree of elements to the parent container
+$board.replaceChildren(...$children);
+
+const $square = document.querySelectorAll('.square');
